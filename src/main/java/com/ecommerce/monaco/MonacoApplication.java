9436b11e0f52b1210c6035dbacaa427b25d7bd0a
avishjainaj <avishj21@gmail.com>
2024-12-28 10:40:52 +0530

    Project Initialisation

---
+++ src/main/java/com/ecommerce/monaco/MonacoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.monaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/monaco/MonacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.monaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
